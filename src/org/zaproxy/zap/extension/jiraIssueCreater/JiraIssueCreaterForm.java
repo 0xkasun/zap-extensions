b6178b57a475a4752b13f104a8da31df86d9207b
0xkasun <kmbkck1@gmail.com>
2015-11-17 17:17:41 +0530

    Revert back to the old form with comments due to netbeans errors.

--- src/org/zaproxy/zap/extension/jiraIssueCreater/JiraIssueCreaterForm.java
+++ src/org/zaproxy/zap/extension/jiraIssueCreater/JiraIssueCreaterForm.java
@@ -25,60 +25,13 @@ import java.util.Properties;
  */
 public class JiraIssueCreaterForm extends javax.swing.JFrame {
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnCancel;
-    private javax.swing.JButton btnExport;
-    private javax.swing.JCheckBox cbHigh;
-    private javax.swing.JCheckBox cbLow;
-    private javax.swing.JCheckBox cbMedium;
-    private javax.swing.JComboBox cbProjectKeys;
-    private javax.swing.JComboBox cbSelectAssignee;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JPanel jPanel1;
     /**
      * Creates new form JiraIssueCreaterForm
      */
     public JiraIssueCreaterForm() {
         initComponents();
         this.listJiraProjects();
-
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(JiraIssueCreaterForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(JiraIssueCreaterForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(JiraIssueCreaterForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(JiraIssueCreaterForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new JiraIssueCreaterForm().setVisible(true);
-            }
-        });
+        
     }
 
     /**
@@ -310,6 +263,7 @@ public class JiraIssueCreaterForm extends javax.swing.JFrame {
 
     }//GEN-LAST:event_cbSelectAssigneeActionPerformed
 
+
     private void getAssignees(String project) { //get a list of assignees for a project
         Properties prop = new Properties();
         InputStream input = null;
@@ -323,10 +277,10 @@ public class JiraIssueCreaterForm extends javax.swing.JFrame {
                     "/multiProjectSearch?projectKeys=" + project);
             JSONArray assigneeArray = new JSONArray(asignees);
 
-            if(cbSelectAssignee.getSelectedItem()!=null){ //to stop regenerating the items in list
+            if(cbSelectAssignee.getSelectedItem()!=null){ //to stop regenrating users
                 cbSelectAssignee.removeAllItems();
             }
-
+            
             for (int i = 0; i < assigneeArray.length(); i++) {
                 JSONObject user = assigneeArray.getJSONObject(i);
                 cbSelectAssignee.addItem(user.getString("name"));
@@ -382,5 +336,55 @@ public class JiraIssueCreaterForm extends javax.swing.JFrame {
             e.printStackTrace();
         }
     }
+
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JiraIssueCreaterForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JiraIssueCreaterForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JiraIssueCreaterForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JiraIssueCreaterForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JiraIssueCreaterForm().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnExport;
+    private javax.swing.JCheckBox cbHigh;
+    private javax.swing.JCheckBox cbLow;
+    private javax.swing.JCheckBox cbMedium;
+    private javax.swing.JComboBox cbProjectKeys;
+    private javax.swing.JComboBox cbSelectAssignee;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
